2f77d076fb38c1932f4fd86c9a252b0527aa3676
twenty0ne <twenty0ne@qq.com>
2019-04-13 07:45:11 +0800

    update

---
+++ knapsack problem/main.py
@@ -0,0 +1,9 @@
+
+[]
+def knapsack(cc, ):
+	pass
+
+if __name__ == "__main__":
+	# print('hello')
+
+	
--- leetcode/322_Coin_Change.py
+++ leetcode/322_Coin_Change.py
@@ -1,10 +1,27 @@
 # https://leetcode.com/problems/coin-change/
 
+def _coinChange(idxCoin, coins, amount):
+	if amount == 0:
+		return 0
+	if idxCoin < len(coins) and amount > 0:
+		maxVal = int(amount / coins[idxCoin])
+		minCost = amount + 1
+		for x in range(0, maxVal + 1):
+			kk = x * coins[idxCoin]
+			if amount >= x * coins[idxCoin]:
+				res = _coinChange(idxCoin + 1, coins, amount - x * coins[idxCoin])
+				if res != -1:
+					minCost = min(minCost, res + x)
+		return -1 if (minCost == amount + 1) else minCost
+	return -1
+
 def coinChange(coins, amount):
-	return 0
+	return _coinChange(0, coins, amount)
 
 if __name__ == "__main__":
-	ret =	coinChange([1,2,5], 11)
-	assert ret == 3
-	ret = coinChange([2], 3)
-	assert ret == -1
+	res =	coinChange([1,2,5], 11)
+	print('result > ' + str(res)) 
+	# assert res == 3
+	res = coinChange([2], 3)
+	print('result > ' + str(res))
+	# assert ret == -1
