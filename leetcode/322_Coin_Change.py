3b757da993f889ff1e432632f18263729bc4871b
twenty0ne <twenty0ne@qq.com>
2019-04-14 22:33:04 +0800

    update

--- leetcode/322_Coin_Change.py
+++ leetcode/322_Coin_Change.py
@@ -17,13 +17,23 @@ def _coinChange(idxCoin, coins, amount):
 		return -1 if (minCost == amount + 1) else minCost
 	return -1
 
-def coinChange(coins, amount):
+def coinChange1(coins, amount):
 	return _coinChange(0, coins, amount)
 
+def coinChange2(coins, amount):
+	max = amount + 1
+	dp = [max for i in range(amount + 1)]
+	dp[0] = 0
+	for i in range(1, amount + 1):
+		for j in range(0, len(coins)):
+			if coins[j] <= i:
+				dp[i] = min(dp[i], dp[i - coins[j]] + 1)
+	return -1 if dp[amount] > amount else dp[amount]
+
 if __name__ == "__main__":
-	res =	coinChange([1,2,5], 11)
+	res =	coinChange2([1,2,5], 11)
 	print('result > ' + str(res)) 
 	# assert res == 3
-	res = coinChange([2], 3)
+	res = coinChange2([2], 3)
 	print('result > ' + str(res))
 	# assert ret == -1
