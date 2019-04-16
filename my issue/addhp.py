eaeca074c14979dfb60bb85ee8b1f5f0724f27c6
twenty0ne <twenty0ne@qq.com>
2019-04-16 14:04:13 +0800

    addhp

--- my issue/addhp.py
+++ my issue/addhp.py
@@ -7,64 +7,59 @@
 属于 动态规划 的问题
 
 输入：
-100血量 x 7个，500血量 x 3个，1000血量 x 1个，需要恢复 750 血量
+100血量 x 7个，500血量 x 3个，1000血量 x 1个
 
 输出：
-500x1 + 100x3
+750 = 500x1 + 100x3
+600 = 500x1 + 100x1
+70 = 100x1
+1200 = 1000x1 + 100x2
 """
 
-	# if amount == 0:
-	# 	return 0
-	# if idxCoin < len(coins) and amount > 0:
-	# 	maxVal = int(amount / coins[idxCoin])
-	# 	minCost = amount + 1
-	# 	print("-------------")
-	# 	for x in range(0, maxVal + 1):
-	# 		kk = amount - x * coins[idxCoin]
-	# 		if amount >= x * coins[idxCoin]:
-	# 			res = _coinChange(idxCoin + 1, coins, amount - x * coins[idxCoin])
-	# 			if res != -1:
-	# 				print('select > ' + str(coins[idxCoin]))
-	# 				minCost = min(minCost, res + x)
-	# 	return -1 if (minCost == amount + 1) else minCost
-	# return -1
+import math
 
 # return select idx num
-def addhp(idx, items, nums, hp, add):
+def addhp(idx, items, nums, hp, total, select):
 	if hp <= 0:
 		return 0
 	if idx < len(items) and hp > 0:
 		# if nums[idx] <= 0:
 		# 	return addhp(idx + 1, items, nums, hp, select)
 		hv = items[idx]
-		maxVal = int(hp / hv)
+		maxVal = math.ceil(hp / hv)
 		maxVal = min(maxVal, nums[idx])
 		minCost = hp * 2
-		sx = 0
+		sx = -1
+		maxTotal = 99999
+		tmpSelect2 = {}
 		for x in range(0, maxVal + 1):
 			if idx == 0:
 				print("-------------------- " + str(x))
-			if hp >= x * hv:
-				res = addhp(idx + 1, items, nums, hp - x * hv, x * hv)
-				print("xx-- x:" + str(x) + " idx:" + str(idx + 1) + " hp:" + str(hp - x * hv) + " res:" + str(res))
-				if res != -1:
-					minCost = min(minCost, res + x)
-		return -1 if (minCost > hp) else minCost
-	return -1
+			# if hp >= x * hv:
+			# lastTotal += x * hv
+			tmpSelect = {}
+			tmpSelect[hv] = x
 
-# def addhp(items, nums, amount):
-# 	max = amount + 1
-# 	dp = [max for i in range(amount + 1)]
-# 	dp[0] = 0
-# 	for i in range(1, amount + 1):
-# 		print("-------------------")
-# 		for j in range(0, len(coins)):
-# 			if coins[j] <= i:
-# 				dp[i] = min(dp[i], dp[i - coins[j]] + 1)
-# 				print("select > " + str(dp[i]))
-# 	return -1 if dp[amount] > amount else dp[amount]
+			res = addhp(idx + 1, items, nums, hp - x * hv, total + x * hv, tmpSelect)
+			print("xx-- x:" + str(x) + " idx:" + str(idx + 1) + " hp:" + str(hp - x * hv) + " res:" + str(res))
+			if res != -1 and res + x * hv >= hp and total + res + x * hv < maxTotal:
+				maxTotal = total + res + x * hv
+				sx = res + x * hv
+				tmpSelect2 = tmpSelect
+		# select = dict(select, **tmpSelect2)
+		for k in tmpSelect2:
+			if k not in select:
+				select[k] = 0
+			select[k] += tmpSelect2[k]
+		return sx
+		# return -1 if (minCost > hp) else minCost
+	return 0
 
 if __name__ == "__main__":
-	select = []
-	res = addhp(0, [100, 500, 1000], [7, 3, 1], 750, 0)
+	select = {}
+	res = addhp(0, [100, 500, 1000], [7, 3, 1], 750, 0, select)
+	print("result > " + str(res))
+	
+	select = {}
+	res = addhp(0, [100, 500, 1000], [0, 1, 1], 1200, 0, select)
 	print("result > " + str(res))
