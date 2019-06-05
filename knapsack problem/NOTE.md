8397b1789dc058a39db941395b824653888b080a
twenty0ne <twenty0ne@qq.com>
2019-06-05 09:08:55 +0800

    update addhp & knapsack problem

--- knapsack problem/NOTE.md
+++ knapsack problem/NOTE.md
@@ -1,2 +1,11 @@
 ## LINK:  
 https://blog.csdn.net/chanmufeng/article/details/82955730  
+
+多重背包：经典DP问题( 基本/二进制优化/单调队列优化 )
+https://blog.csdn.net/belous_zxy/article/details/80357450
+
+从背包问题优化详解动态规划思想
+https://blog.csdn.net/qq_39630587/article/details/79134217
+
+【动态规划】一次搞定三种背包问题
+https://cloud.tencent.com/developer/article/1432364
--- my issue/addhp.py
+++ my issue/addhp.py
@@ -4,7 +4,7 @@
 选出最合适的组合
 
 分析：
-属于 动态规划 的问题
+属于 背包问题，使用 动态规划 优化时间
 
 输入：
 100血量 x 7个，500血量 x 3个，1000血量 x 1个
@@ -17,43 +17,38 @@
 """
 
 import math
+import sys
 
-# return select idx num
+# return idx offer value
 def addhp(idx, items, nums, hp, total, select):
-	if hp <= 0:
+	if hp <= 0 or idx >= len(items):
 		return 0
-	if idx < len(items) and hp > 0:
-		# if nums[idx] <= 0:
-		# 	return addhp(idx + 1, items, nums, hp, select)
-		hv = items[idx]
-		maxVal = math.ceil(hp / hv)
-		maxVal = min(maxVal, nums[idx])
-		minCost = hp * 2
-		sx = -1
-		maxTotal = 99999
-		tmpSelect2 = {}
-		for x in range(0, maxVal + 1):
-			if idx == 0:
-				print("-------------------- " + str(x))
-			# if hp >= x * hv:
-			# lastTotal += x * hv
-			tmpSelect = {}
-			tmpSelect[hv] = x
+	if idx in select and hp in select[idx]:
+		return select[idx][hp][0] # return offerVal
 
-			res = addhp(idx + 1, items, nums, hp - x * hv, total + x * hv, tmpSelect)
-			print("xx-- x:" + str(x) + " idx:" + str(idx + 1) + " hp:" + str(hp - x * hv) + " res:" + str(res))
-			if res != -1 and res + x * hv >= hp and total + res + x * hv < maxTotal:
-				maxTotal = total + res + x * hv
-				sx = res + x * hv
-				tmpSelect2 = tmpSelect
-		# select = dict(select, **tmpSelect2)
-		for k in tmpSelect2:
-			if k not in select:
-				select[k] = 0
-			select[k] += tmpSelect2[k]
-		return sx
-		# return -1 if (minCost > hp) else minCost
-	return 0
+	hv = items[idx]
+	maxVal = math.ceil(hp / hv)
+	maxVal = min(maxVal, nums[idx])
+	offerVal = -1
+	maxOfferTotal = sys.maxsize
+
+	for x in range(0, maxVal + 1):
+		# if idx == 0:
+		# 	print("-------------------- " + str(x))
+		curCost = x * hv
+		if total + curCost >= maxOfferTotal:
+			continue 
+
+		res = addhp(idx + 1, items, nums, hp - curCost, total + x * hv, select)
+		print("xx-- x:" + str(x) + " idx:" + str(idx + 1) + " hp:" + str(hp - x * hv) + " res:" + str(res))
+		if res != -1 and res + x * hv >= hp and total + res + x * hv < maxOfferTotal:
+			maxOfferTotal = total + res + x * hv
+			offerVal = res + x * hv
+			select[idx] = {
+				hp: [offerVal, x]
+			}
+			
+	return offerVal
 
 if __name__ == "__main__":
 	select = {}
